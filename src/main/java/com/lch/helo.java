29fad1ed01dbcb61b2ed2bd29128ca1507494396
lch_pro <121922028@qq.com>
2020-07-13 15:18:36 +0800

    第一次提交呢

---
+++ src/main/java/com/lch/helo.java
@@ -0,0 +1,11 @@
+package com.lch;
+
+/**
+ * @author lch
+ * @Date 2020/7/13/013
+ */
+public class helo {
+    public static void main(String[] args) {
+        System.out.println("这就是男人");
+    }
+}
