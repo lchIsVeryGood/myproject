82d67c5dd08eba95205e56077035e9c2e8849a37
lch_pro <121922028@qq.com>
2020-07-13 15:23:13 +0800

    第二次提交呢

--- src/main/java/com/lch/helo.java
+++ src/main/java/com/lch/helo.java
@@ -8,4 +8,7 @@ public class helo {
     public static void main(String[] args) {
         System.out.println("这就是男人");
     }
+    public static void say(){
+        System.out.println("我爱你狗蛋");
+    }
 }
